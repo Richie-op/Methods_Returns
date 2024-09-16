05dbd77e659e0a3e613e050e4d5eb1bf17f3c9fd
richardsonnoel01 <richardsonnoel01@gmail.com>
2024-09-15 22:49:09 -0400

    User

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        User String = new User("Richardson", "Noel", "Richie302@gmail.com",
+                "Noel32Richie");
+
+        System.out.println(String.firstName);
+        System.out.println(String.lastName);
+        System.out.println(String.email);
+        System.out.println(String.password);
+    }
+
+    public static void investmentApp(String[] args) {
+
+    }
+}
